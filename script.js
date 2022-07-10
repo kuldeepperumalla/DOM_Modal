f0b38aa53635ea9baa9ceda58997f2ce9e13d6ad
kuldeepperumalla <kuldeepperumalla325@gmail.com>
2022-07-10 14:01:22 -0400

    Added button functionality

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 'use strict';
 
 const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.close');
+const overlay = document.querySelector('.close-modal');
 const btn = document.querySelector('.show-modal');
 
 btn.addEventListener('click', function () {
-    alert('hi')
+    modal.classList.remove("hidden");
+})
+
+overlay.addEventListener('click', function () {
+    modal.classList.add("hidden");
 })
