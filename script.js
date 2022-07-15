31d3914d439de32149646d62d78d91cfd203405a
kuldeepperumalla <kuldeepperumalla325@gmail.com>
2022-07-14 23:33:58 -0400

    added close functionality

--- script.js
+++ script.js
@@ -4,10 +4,21 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
+var statusCheck = false;
+
+var updateStatus = (e) => {
+    if (e) {
+        modal.classList.remove("hidden");
+    } else modal.classList.add("hidden");
+};
 
 for (let i = 0; btnsOpenModal.length > i; i++) {
-    btnsOpenModal[i].addEventListener('click', function () {
+    btnsOpenModal[i].addEventListener("click", function () {
         console.log(`Button ${btnsOpenModal[i].textContent} Clicked`);
-        modal.classList.remove('hidden')
+        updateStatus(true);
     });
 }
+
+btnCloseModal.addEventListener("click", function () {
+    updateStatus(false);
+});
